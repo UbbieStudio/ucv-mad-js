005e4466e10f099dac99b1c7db878955415e3b0f
ubbie <bgalan@ubbie.studio>
2024-10-09 21:27:12 +0200

    Adding clickers and emoji color changer

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,3 +14,14 @@ window.onload = function () {
     console.log("Hello, world!");
     console.log(Math.random());
 }
+
+function changeEmojiColor(elem) {
+    const r = randInt(0, 255);
+    const g = randInt(0, 255);
+    const b = randInt(0, 255);
+    elem.style.fill = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
+
+function randInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
