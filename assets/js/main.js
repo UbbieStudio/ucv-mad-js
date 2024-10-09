07be1414e93b27f7dcb8e5feb6c29298fc1176ea
ubbie <bgalan@ubbie.studio>
2024-10-09 23:32:06 +0200

    On Mouse Enter and Leave Events

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,14 @@ const dice = [
     'M3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3zm1 5.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m8 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3m1.5 6.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M12 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3M5.5 12a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0M4 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3'
 ];
 
+function isFocused(elem) {
+    elem.style.fill = 'orange';
+}
+
+function isNotFocused(elem) {
+    elem.style.fill = 'white';
+}
+
 function throwDie(elem) {
     const rand = randInt(0, 5);
     elem.children[0].setAttribute('d', dice[rand]);
