c30fa75c3680a633d483c1875267440a7063ef4e
ubbie <bgalan@ubbie.studio>
2024-10-18 11:55:20 +0200

    Theme toggle implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,7 +35,7 @@ const dice = [
 ];
 
 function isFocused(elem) {
-    elem.style.fill = 'orange';
+    elem.style.fill = '#68c33f';
 }
 
 function isNotFocused(elem) {
@@ -47,7 +47,24 @@ function throwDie(elem) {
     elem.children[0].setAttribute('d', dice[rand]);
 }
 
-function toggleTheme() {
+const day_night = [
+    "M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278",
+    "M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"
+];
+
+function toggleTheme(elem) {
+
+    // Changing button's icon (moon/sun) and it's color
+    if (elem.children[0].getAttribute('d') == day_night[0]) {
+        elem.children[0].setAttribute('d', day_night[1]);
+        elem.children[0].style.fill = "#fbb301";
+    }
+    else {
+        elem.children[0].setAttribute('d', day_night[0]);
+        elem.children[0].style.fill = "#b6cdff";
+    }
+
+    // Changing main's background image
     const main = document.getElementsByTagName('main')[0];
     if (main.style.backgroundImage == "") {
         main.style.backgroundImage = "url('../assets/img/moonlit-lake.jpg')";
@@ -58,4 +75,31 @@ function toggleTheme() {
     else {
         main.style.backgroundImage = main.style.backgroundImage.replace("park-sunset", "moonlit-lake");
     }
+
+    const header = document.getElementsByTagName('header')[0];
+    const title = document.querySelector("header h1");
+    const footer = document.getElementsByTagName('footer')[0];
+
+    if (header.style.backgroundColor == "") {
+        header.style.backgroundColor = "rgb(43, 46, 54)";
+        footer.style.backgroundColor = "rgb(43, 46, 54)";
+        header.style.color = "white";
+        title.style.color = "white";
+        footer.style.color = "white";
+    }
+    else if (header.style.backgroundColor == "rgb(43, 46, 54)") {
+        header.style.backgroundColor = "white";
+        footer.style.backgroundColor = "white";
+        header.style.color = "black";
+        title.style.color = "black";
+        footer.style.color = "black";
+    }
+    else {
+        header.style.backgroundColor = "rgb(43, 46, 54)";
+        footer.style.backgroundColor = "rgb(43, 46, 54)";
+        header.style.color = "white";
+        title.style.color = "white";
+        footer.style.color = "white";
+    }
+
 }
