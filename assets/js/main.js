519b12c47144349e3a29c19b764fac6769a6cd0d
ubbie <bgalan@ubbie.studio>
2024-11-14 13:27:08 +0100

    Random commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 /* VARIABLES Y CONSTANTES */
 
-const day_night = [
+const diaNoche = [
     "M6 .278a.77.77 0 0 1 .08.858 7.2 7.2 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277q.792-.001 1.533-.16a.79.79 0 0 1 .81.316.73.73 0 0 1-.031.893A8.35 8.35 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.75.75 0 0 1 6 .278",
     "M8 12a4 4 0 1 0 0-8 4 4 0 0 0 0 8M8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0m0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13m8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5M3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8m10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0m-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0m9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707M4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708"
 ];
@@ -78,29 +78,47 @@ function cambiarTema(elem) {
 
 
     if (tema == 'claro') {
+        // Cambio color de fondo y color de texto de la cabecera
         cabecera.style.backgroundColor = "rgb(43, 46, 54)";
         cabecera.style.color = "white";
 
+        // Cambio color de fondo y color de texto del pie de página
         pieDePagina.style.backgroundColor = "rgb(43, 46, 54)";
         pieDePagina.style.color = "white";
 
+        // Cambio color de texto del título de la página
         titulo.style.color = "white";
 
+        // Cambio de la imagen de fondo del contenedor "main"
         principal.style.backgroundImage = "url('../assets/img/moonlit-lake.jpg')";
 
+        // Cambio icono luna por icono sol
+        elem.children[0].setAttribute('d', diaNoche[1]);
+        elem.children[0].style.fill = "#fbb301";
+
+        // Cambio el valor de mi variable auxiliar "tema"  para alternar entre tema claro y oscuro
         tema = 'oscuro';
     }
     else {
+        // Cambio color de fondo y color de texto de la cabecera
         cabecera.style.backgroundColor = "white";
         cabecera.style.color = "black";
 
+        // Cambio color de fondo y color de texto del pie de página
         pieDePagina.style.backgroundColor = "white";
         pieDePagina.style.color = "black";
 
+        // Cambio color de texto del título de la página
         titulo.style.color = "black";
 
+        // Cambio de la imagen de fondo del contenedor "main"
         principal.style.backgroundImage = "url('../assets/img/park-sunset.jpg')";
 
+        // Cambio icono sol por icono luna
+        elem.children[0].setAttribute('d', diaNoche[0]);
+        elem.children[0].style.fill = "#b6cdff";
+
+        // Cambio el valor de mi variable auxiliar "tema"  para alternar entre tema claro y oscuro
         tema = 'claro';
     }
 
